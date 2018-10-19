3cee9a5fbeba8aeafc2c58dca04066bfb82418fa
johnsontaiwo <josseytee@gmail.com>
2018-10-19 14:00:14 -0400

    modified App.jd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        
           <p>
             Let's Talk
           </p>
