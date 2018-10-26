c4a07e44e04a30504d70623e06c0b46a09e24faf
johnsontaiwo <josseytee@gmail.com>
2018-10-26 13:16:41 -0400

    installed react-router-dom

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //import rootReducer from './Reducers'
 
 
